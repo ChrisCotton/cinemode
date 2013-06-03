b5a0c029fc69ee2baf842a72386c559f7fa5dc4f
Jack Tang <jackhftang@hotmail.com>
2013-06-03 09:00:54 +0800

    Mon Jun  3 09:00:54 HKT 2013

--- utility/setup_db.sql
+++ utility/setup_db.sql
@@ -31,50 +31,50 @@ create table if not exists products(
   primary key(id)
 );
 
-create table if not exists videos(
-  id            int               auto_increment,
-  url           text              not null,
-  description   text              default "",
+-- create table if not exists videos(
+--   id            int               auto_increment,
+--   url           text              not null,
+--   description   text              default "",
   
-  primary key(id)
-);
+--   primary key(id)
+-- );
 
--- ### Properties
--- + there is always a identifer for public
-create table group(
-  id            int               auto_increment,
-  identifier    varchar(50)       not null,
+-- -- ### Properties
+-- -- + there is always a identifer for public
+-- create table group(
+--   id            int               auto_increment,
+--   identifier    varchar(50)       not null,
   
-  primary key(id),
-  index(indentifier)
-)
+--   primary key(id),
+--   index(indentifier)
+-- )
 
-create table groups_of_videos(
-  group_id
-);
+-- create table groups_of_videos(
+--   group_id
+-- );
 
--- ### Constraints
--- + add/remove row should update products.like_count
---      select count(*) from users_like_products where product_id = x == select like_count from products where id = x
-create table if not exists users_like_products(
-  user_id     int not null,
-  product_id  int not null,
+-- -- ### Constraints
+-- -- + add/remove row should update products.like_count
+-- --      select count(*) from users_like_products where product_id = x == select like_count from products where id = x
+-- create table if not exists users_like_products(
+--   user_id     int not null,
+--   product_id  int not null,
   
-  index(user_id),
-  index(product_id),
-  foreign key(user_id)    references users(id)     on delete cascade,
-  foreign key(product_id) references products(id)  on delete cascade 
-);
+--   index(user_id),
+--   index(product_id),
+--   foreign key(user_id)    references users(id)     on delete cascade,
+--   foreign key(product_id) references products(id)  on delete cascade 
+-- );
 
-create table if not exists products_in_videos(
-  video_id    int       not null,
-  product_id  int       not null,
-  start_time  double    not null,     -- duration of showing video
-  end_time    double    not null,     
-  rank        int       default 0,    -- for sorting from top to bottom
+-- create table if not exists products_in_videos(
+--   video_id    int       not null,
+--   product_id  int       not null,
+--   start_time  double    not null,     -- duration of showing video
+--   end_time    double    not null,     
+--   rank        int       default 0,    -- for sorting from top to bottom
   
-  index(video_id),
-  index(product_id),
-  foreign key(video_id)   references videos(id)     on delete cascade,
-  foreign key(product_id) references products(id)   on delete cascade
-);
+--   index(video_id),
+--   index(product_id),
+--   foreign key(video_id)   references videos(id)     on delete cascade,
+--   foreign key(product_id) references products(id)   on delete cascade
+-- );
